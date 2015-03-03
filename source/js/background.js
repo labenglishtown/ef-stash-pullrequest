376180f03a9d62c3f54612b11d6450c8ce0d9d7b
leo.hecl <leo.hecl@ef.com>
2015-03-03 14:58:19 +0800

    remote unnecessary comments

--- source/js/background.js
+++ source/js/background.js
@@ -1,11 +1,5 @@
-
-
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
 
     if (request.newTab){
     	chrome.tabs.create({ url: request.url , active: false});
